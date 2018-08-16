3bd46f13ceee434a6b1c33bb8e67435b78309176
Bruno Bentzen <b.bentzen@hotmail.com>
2018-08-17 01:13:09 +0800

    fixed definition

--- semantics/kripke/basic.lean
+++ semantics/kripke/basic.lean
@@ -6,22 +6,21 @@ Author: Bruno Bentzen
 
 import ..default
 
+open classical
+
 /- Kripke models -/
 
-definition frame : Type := (nat × (nat → nat → bool))
+definition frame : Type := (list nat) × (nat → nat → Prop)
 
-definition k_model : Type := frame × (nat → nat → bool)
+definition k_model : Type := frame × (nat → nat → Prop)
 
 notation `𝓦` `⸴` `𝓡` `⸴` `𝓿` := k_model
 
-def true_in_wrld (M : (𝓦 ⸴ 𝓡 ⸴ 𝓿)) : form → nat → bool
-| # p     := λ w, M.snd w p --nat.rec_on (M.fst.fst) tt (λ _ _, M.snd w p)
-| ~ p     := λ w, bnot (true_in_wrld p w)
-| (p ⊃ q) := λ w, (bnot (true_in_wrld p w)) || (true_in_wrld q w) 
+def true_in_wrld (M : (𝓦 ⸴ 𝓡 ⸴ 𝓿)) : form → nat → Prop
+| # p     := λ w, M.snd w p
+| ~ p     := λ w, ¬ (true_in_wrld p w)
+| (p ⊃ q) := λ w, (true_in_wrld p w) → (true_in_wrld q w) 
 | ◻ p    := λ w, 
-    nat.rec_on M.fst.fst tt 
-    (λ v IH, IH && ((bnot (M.fst.snd w v)) || (true_in_wrld p v)))
+  (∀ v : nat, (v ∈ M.fst.fst) → (M.fst.snd w v) → (true_in_wrld p v))
 
 notation M `⦃`p`⦄` w := true_in_wrld M p w
-
--- W wrld is a n ∈ nat s.t. w ∈ wrld iff w ≤ wrld
