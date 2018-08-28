1f27848e7d1de60e5effd937bf908132d4655878
Bruno Bentzen <b.bentzen@hotmail.com>
2018-08-28 22:54:46 +0800

    Delete default.lean

--- semantics/default.lean
+++
@@ -1,8 +0,0 @@
-/-
-Copyright (c) 2017 Bruno Bentzen. All rights reserved.
-Released under the Apache License 2.0 (see "License");
-Author: Bruno Bentzen
--/
-
-import ..syntax.basic ..syntax.language.basic
-
